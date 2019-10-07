a4d2412ee97316270cb4cccd510a36e62567d129
Evgeniy Gushchin <evgeniy.gushchin@aldigit.com>
2019-10-07 16:10:50 +0600

    - add controllers from Data structures profiler - add Suffix Sequence and Iterator

---
+++ HomeworkStoryboard/ViewControllers/Feed/FeedViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  FeedViewModel.swift
+//  HomeworkStoryboard
+//
+//  Created by Evgeniy Gushchin on 07/10/2019.
+//  Copyright © 2019 EG. All rights reserved.
+//
+
+import UIKit
+
+struct FeedViewModel {
+    
+    let dataSource = FeedDataProvider()
+    
+    func numberOfCells() -> Int {
+        return dataSource.feedMockData().count
+    }
+    
+    func valueForCell(indexPath: IndexPath) -> String {
+        guard indexPath.row < dataSource.feedMockData().count else {
+            fatalError("index out of bound")
+        }
+        return dataSource.feedMockData()[indexPath.row].name
+    }
+    
+    func viewController(indexPath: IndexPath) -> UIViewController? {
+        let storyboard = UIStoryboard(name: "DataStructures", bundle: nil)
+        var vc: UIViewController?
+        
+        let name = valueForCell(indexPath: indexPath)
+        
+        switch name {
+        case "Array":
+            vc = storyboard.instantiateViewController(withIdentifier: "ArrayViewController")
+        case "Set":
+            vc = storyboard.instantiateViewController(withIdentifier: "SetViewController")
+        case "Dictionary":
+            vc = storyboard.instantiateViewController(withIdentifier: "DictionaryViewController")
+        case "SuffixArray":
+            let storyboard = UIStoryboard(name: "DataStructures", bundle: nil)
+            // TODO
+            vc = storyboard.instantiateViewController(withIdentifier: "DictionaryViewController")
+        default:
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            vc = storyboard.instantiateViewController(withIdentifier: "BSceneController")
+        }
+        return vc
+    }
+
+}
