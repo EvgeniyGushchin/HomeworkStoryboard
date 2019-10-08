9326a17b5a471808f0aaf204dc6cc253b7c53737
Evgeniy Gushchin <evgeniy.gushchin@aldigit.com>
2019-10-08 16:33:03 +0600

    add Phone Mask

--- HomeworkStoryboard/ViewControllers/ProfileViewController.swift
+++ HomeworkStoryboard/ViewControllers/ProfileViewController.swift
@@ -12,15 +12,37 @@ class ProfileViewController: UIViewController {
     
     weak var delegate: UIViewController?
         
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
+    @IBOutlet weak var phoneNumberField: UITextField!
+    //    override var preferredStatusBarStyle: UIStatusBarStyle {
+//        return .lightContent
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addBehaviors(behaviors: [NightModeBehavior()])
+        let mask = PhoneMask(maskString: "+7 (000) 000 00 00")
+        var formatted = mask.mask(string: "95")
+        for i in 0..<6 {
+            let next2 = formatted + "\(i)"
+            formatted = mask.mask(string: next2)
+        }
+        phoneNumberField.attributedText = mask.fullAttributedMask(string: "8782gg77")
+//        addBehaviors(behaviors: [NightModeBehavior()])
+    }
+    
+    @IBAction func textFieldChanged(_ sender: UITextField) {
+        
+    }
+}
+
+extension ProfileViewController: UITextFieldDelegate {
+    
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        textField.text = "+7 ("
+//    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return true
     }
     
 }
-
